79109fd539d23a28082dfaf58bdcb1f884d94e92
James Cho <hwisoocho@gmail.com>
2019-01-18 09:50:19 -0800

    query by keyword and return a list

---
+++ src/Doctor.js
@@ -0,0 +1,41 @@
+import { API_KEY } from '../.env';
+export class Doctor {
+  constructor(keyword) {
+    this.keyword = keyword;
+    this.location = "47.6062%2C-122.3321"
+  }
+
+  keyWordSearch() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.keyword}&user_location=${this.location}&skip=0&limit=10&user_key=${API_KEY}`
+
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  getDetails(id) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${id}&skip=0&limit=1&user_key=${API_KEY}`
+
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,3 +2,42 @@ import { Doctor } from './Doctor.js';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function () {
+  $("#conditionButton").click(function (event) {
+    event.preventDefault();
+    $("#results").empty();
+    $("#results").html("<h2>Search Results</h2>");
+    let condition = $("#condition").val();
+    let newSearch = new Doctor(condition);
+    let promise = newSearch.keyWordSearch();
+
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      console.table(body);
+      for (let i = 0; i < body.data.length; i++) {
+
+        $("#results").append(`<li id=${body.data[i].profile.slug}>${body.data[i].profile.first_name} ${body.data[i].profile.last_name} ${body.data[i].profile.title}</li>`);
+      }
+      $("li").click(function (event) {
+        $("#details").empty();
+        $("#detail").html("<h2>Doctor Details</h2>");
+        let id = this.getAttribute("id");
+
+        let detailsPromise = newSearch.getDetails(id);
+        detailsPromise.then(function (response) {
+          let body = JSON.parse(response);
+
+          $("#details").append(`<img class='img-thumbnail' src=${body.data[0].profile.image_url} alt='profile'>`);
+          $("#details").append(`<p>Name: ${body.data[0].profile.first_name} ${body.data[0].profile.last_name} ${body.data[0].profile.title}</li>`);
+          $("#details").append(`<p>Bio: ${body.data[0].profile.bio} </li>`);
+
+
+          $("#details").fadeIn();
+
+        })
+
+      })
+    })
+  })
+})
