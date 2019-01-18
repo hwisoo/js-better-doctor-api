86768e2f29ca1895ee907a0ecf2b0d2c76f8c6c0
James Cho <hwisoocho@gmail.com>
2019-01-18 10:19:26 -0800

    add search by name

--- src/Doctor.js
+++ src/Doctor.js
@@ -22,6 +22,24 @@ export class Doctor {
     });
   }
 
+  nameSearch() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = ` https://api.betterdoctor.com/2016-03-01/doctors?name=${this.keyword}&user_location=${this.location}&sort=distance-asc&skip=0&limit=10&user_key=${API_KEY}`
+
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+  }
+
   getDetails(id) {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function () {
+  //Search by Keyword
   $("#conditionButton").click(function (event) {
     event.preventDefault();
     $("#results").empty();
@@ -34,9 +35,43 @@ $(document).ready(function () {
 
 
           $("#details").fadeIn();
-
         })
+      })
+    })
+  })
 
+  //Search by Doctor Name
+  $("#doctorButton").click(function (event) {
+    event.preventDefault();
+    $("#results").empty();
+    $("#results").html("<h2>Search Results</h2>");
+    let doctor = $("#doctor").val();
+    let newSearch = new Doctor(doctor);
+    let promise = newSearch.nameSearch();
+
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      console.table(body);
+      for (let i = 0; i < body.data.length; i++) {
+
+        $("#results").append(`<li id='${body.data[i].profile.first_name} ${body.data[i].profile.last_name}'>${body.data[i].profile.first_name} ${body.data[i].profile.last_name} ${body.data[i].profile.title}</li>`);
+      }
+      $("li").click(function (event) {
+        $("#details").empty();
+        $("#details").html("<h2>Doctor Details</h2>");
+        let id = this.getAttribute("id");
+
+        let detailsPromise = newSearch.getDetails(id);
+        detailsPromise.then(function (response) {
+          let body = JSON.parse(response);
+
+          $("#details").append(`<img class='img-thumbnail' src=${body.data[0].profile.image_url} alt='profile'>`);
+          $("#details").append(`<p>Name: ${body.data[0].profile.first_name} ${body.data[0].profile.last_name} ${body.data[0].profile.title}</li>`);
+          $("#details").append(`<p>Bio: ${body.data[0].profile.bio} </li>`);
+
+
+          $("#details").fadeIn();
+        })
       })
     })
   })
